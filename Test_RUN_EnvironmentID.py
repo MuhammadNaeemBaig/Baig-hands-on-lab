78716a9cec9c60d482ee4461a7fbd27f9e982f64
MuhammadNaeemBaig <157546483+MuhammadNaeemBaig@users.noreply.github.com>
2024-03-12 08:12:29 +1100

    Update Test_RUN_EnvironmentID.py
    modified CLI Script

--- Test_RUN_EnvironmentID.py
+++ Test_RUN_EnvironmentID.py
@@ -5,22 +5,24 @@ url = "https://app.coalescesoftware.io/scheduler/startRun"
 payload = {
     "runDetails": {
         "parallelism": 16,
-        "environmentID": "111",
+        "environmentID": "110",
         "jobID": "27"
     },
     "userCredentials": {
         "snowflakeAuthType": "Basic",
-        "snowflakeUsername": "Naeem",
-        "snowflakePassword": "L@hore#570",
-        "snowflakeWarehouse": "BAIG",
+        "snowflakeUsername": "MIPFlyingDoctorDemo",
+        "snowflakePassword": "Welcome2Snowflake",
+        "snowflakeWarehouse": "COMPUTE_WH",
         "snowflakeRole": "ACCOUNTADMIN"
     }
 }
 headers = {
     "accept": "application/json",
     "content-type": "application/json",
-    "Authorization": "Bearer AMf-vBy0hnGlgI5M4VO9eu1e6a3lBsOEUrqJjMhCLFHdySLs8PuaB3QzyInHlUEFTO5qjhTExbAY2W4I4rf6Y6ztdrg1lHU8Aj3hVtLXsc7k7DMpYXV2u3Spyey_8w2dd4aBqYocFGB3g-Cq_TbfDSNvUAJio0fq8V6M37Oz5k5_LEFr9-OlBVzhtPzefyP8Hsw4LgIiN5i3N8lEAGOfcuzNJiurhhqD8Q"
+    "Authorization": "Bearer AMf-vBxiUfrAbbRvAl2SMA6U9JlO4SVqLrffxM_NvC_mPkFnOG8ix1gn0SQK91ns0whZodOTBRgzVQ59uidJTw9kDLYM9VooAKl7ba56jcnbhh-J0TKtdUdyc17uxOM93l_tXDA5ShAlKxrsXk_anJFY9anRTYmvunmdwjVPxyoZX2MUBZvH2BT2OIVzYdZRYi3hoTvobDeBjS7Gf3rY3zrs6v5cieIb-A"
 }
+
+
 
 response = requests.post(url, json=payload, headers=headers)
 
